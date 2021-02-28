7e1baaca99a4e2abcc222fda075988d070c91ec9
marek-nereca <mareknereca@gmail.com>
2021-02-28 08:31:42 +0100

    Add firewall rule, run linux installation script from wsl, create shortcut on desktop

--- install.ps1
+++ install.ps1
@@ -1,9 +1,24 @@
-# create main directory
+# CREATE MAIN DIRECTORY
 $dirName = ".ubuntu"
 $dir = Join-Path $HOME $dirName
 if(!(Test-Path -Path $dir)){
     new-item -Name $dirName -Path $HOME -ItemType Directory
 }
+
+# FIREWALL
+$confirmation = Read-Host "Add firewall rules? y/n"
+if ($confirmation -eq 'y') {
+    # disable default blocking rule
+    # Disable-NetFirewallRule -DisplayName "VcXsrv windows xserver" -Action Block 
+    echo "Public TCP Firewall rule for 'VcXsrv windows xserver' needs to be disabled manually"
+
+    # allow xserver port in firewall
+    New-NetFirewallRule -DisplayName "vcxsrv-wsl" -Direction Inbound -Protocol TCP -LocalPort 6000-6005 -Action Allow -RemoteAddress "172.16.0.0/12" -Program "C:\program files\vcxsrv\vcxsrv.exe"
+}
+
+# COPY SCRIPTS TO USER HOME
+$scriptDirectory = Split-Path $MyInvocation.MyCommand.Path
+Set-Location $scriptDirectory
 
 copy-item .\reload_vcxsrv.ps1 $dir
 copy-item .\start_gnome_session.sh $dir
@@ -11,18 +26,20 @@ copy-item .\start_gnome_session.ps1 $dir
 copy-item .\start_ubuntu.bat $dir
 copy-item .\install.sh $dir
 
-# CREATE SHORTCUT SCRIPT
+# INSTALL IN WSL
+wsl /bin/bash /mnt/c/Users/$env:username/.ubuntu/install.sh
 
-# Define location variables
-# $shortcut_location = "$env:userprofile\Desktop\Ubuntu.lnk"
-$shortcut_location = "$dir\Ubuntu.lnk"
-# $program_location = "cmd.exe /C ""$dir\start_ubuntu.bat"""
-$program_location = "$dir\start_ubuntu.bat"
+# CREATE SHORTCUT SCRIPT
+# define location variables
+$shortcut_location = "$env:userprofile\Desktop\Ubuntu.lnk"
+$program_location = "cmd.exe"
 $icon_location = "$dir\ubuntu.ico"
 
-# Create shortcut
+# create shortcut
 $object = new-object -comobject wscript.shell
 $shortcut = $object.createshortcut($shortcut_location)
 $shortcut.targetpath = $program_location
 $shortcut.iconlocation = $icon_location
+$shortcut.arguments = " /C ""$dir\start_ubuntu.bat"""
+$shortcut.workingdirectory = $dir
 $shortcut.save()
